eedf754bac91602948bef94e2dfc40f0d8ba83a9
Agwanyanjaba <bonkewycliffe@yahoo.com>
2023-11-07 15:28:44 +0300

    update app to show success message on status code 200

--- src/App.js
+++ src/App.js
@@ -1,63 +1,98 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const App = () => {
   const currentYear = new Date().getFullYear();
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);  
   const [summary, setSummary] = useState({
     missingInTarget: 0,
     missingInSource: 0,
     fieldDiscrepancies: 0,
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8088/v1/data-tool/reconciliation", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({}),
-        });
-        const data = await response.json();
-        setSummary(data);
-      } catch (error) {
-        console.error("Error fetching reconciliation data:", error);
+
+  const handleSubmit = async (e) => {
+    // prevent the page from reloading
+    e.preventDefault();
+
+    // construct form data
+    const formData = new FormData();
+    const sourceFile = e.currentTarget.querySelector('input[name="file"]').files[0];
+    const targetFile = e.currentTarget.querySelector('input[name="file"]').files[1];
+    formData.append('sourceFile', sourceFile, 'source.csv');
+    formData.append('targetFile', targetFile, 'target.csv');
+
+    // make a POST request with Axios
+    try {
+      const res = await axios.post('http://localhost:8088/v1/data-tool/reconciliation', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(res);
+      if (res.status === 200) {
+        setShowSuccessMessage(true);
       }
-    };
 
-    fetchData();
-  }, []);
-
+      const data = res.data;
+      const reconciliationData = {
+        missingInTarget: data[1].split(": ")[1],
+        missingInSource: data[2].split(": ")[1],
+        fieldDiscrepancies: data[3].split(": ")[1],
+      };
+      setSummary(reconciliationData);
+    } catch (error) {
+      console.error('Error fetching reconciliation data:', error);
+    }
+  };
 
   return (
     <div>
-      <div style={{ height: '100px', backgroundColor: 'maroon' }}> {/* Header */}
+      <div style={{ height: '100px', backgroundColor: 'maroon' }}>
+        {/* Header */}
       </div>
       <div style={{ height: '610px', backgroundColor: 'white', display: 'flex' }}>
         {/* Middle Section */}
         <div style={{ display: 'flex', flex: '3', flexDirection: 'row' }}>
-          <div style={{ flex: '3', border: '1px solid black', margin: '10px' }}>
+          <div style={{ flex: '3', border: '1px solid black', margin: '10px' , padding: '10px'}}>
             <h2>Welcome, Upload two files to reconcile...</h2>
             <p>Please select two files</p>
-           
-            <p>Records submitted succssfully</p>
+            <form onSubmit={handleSubmit} encType='multipart/form-data'>
+              <input type='file' name='file' multiple />
+              <button type='submit'>Submit</button>
+            </form>
+            <div>{showSuccessMessage && <p>Files submitted successfully</p>}</div>
           </div>
-          <div style={{ flex: '3', border: '1px solid black', margin: '10px' }}>
+          <div style={{ flex: '3', border: '1px solid black', margin: '10px', padding: '10px' }}>
             <h2>Reconciliation Summary</h2>
             <p>Records missing in target: {summary.missingInTarget}</p>
             <p>Records missing in source: {summary.missingInSource}</p>
             <p>Records with field discrepancies: {summary.fieldDiscrepancies}</p>
           </div>
-          <div style={{ flex: '4', border: '1px solid black', margin: '10px' }}>
+          <div style={{ flex: '4', border: '1px solid black', margin: '10px', padding: '10px' }}>
             <h2>Visualization</h2>
           </div>
         </div>
       </div>
 
-      <div style={{ position: 'fixed', bottom: 0, width: '100%', height: '50px', backgroundColor: 'black', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        &copy; {currentYear} John
+      <div
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          width: '100%',
+          height: '50px',
+          backgroundColor: 'black',
+          color: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        &copy; {currentYear} Wycliffe O. 
       </div>
     </div>
   );
 };
+
 export default App;
